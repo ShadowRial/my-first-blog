948395ff15a0fe5fa0ace2caee6ab3a7e4f19023
ShadowRial <shadowrial@mail.ru>
2022-02-01 16:22:54 +0400

    urls + settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -117,13 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [
-
-    ("downloads", "/opt/webfiles/stats")
-
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static', 'static_files'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
